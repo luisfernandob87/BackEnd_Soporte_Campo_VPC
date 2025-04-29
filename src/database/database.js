b4fea755c54b09629f6445a3228032a4aeafd914
Junior <luisfernandob87@gmail.com>
2025-04-28 19:59:22 -0600

    add sslmode

--- src/database/database.js
+++ src/database/database.js
@@ -5,7 +5,13 @@ const Sequelize = require('sequelize')
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 })
 
 module.exports = { sequelize }
