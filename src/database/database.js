e75885f7808584ff7c5d84b9d3c641fa19c59145
Luis Morales <luisfernandob87@gmail.com>
2026-09-16 10:25:48 -0600

    Change database dialect from MSSQL to PostgreSQL
    Updated database connection settings to use PostgreSQL instead of MSSQL.

--- src/database/database.js
+++ src/database/database.js
@@ -1,34 +1,34 @@
 
 // PostgrestSQL
-// const Sequelize = require('sequelize')
-
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     dialect: 'postgres',
-//     dialectOptions: {
-//         ssl: {
-//             require: true,
-//             rejectUnauthorized: false
-//         }
-//     }
-// })
-
-// module.exports = { sequelize }
-
-const { Sequelize } = require('sequelize');
-
+const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    dialect: 'mssql',
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
+    dialect: 'postgres',
     dialectOptions: {
-        options: {
-            encrypt: true,
-            trustServerCertificate: true
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
         }
     }
 })
 
 module.exports = { sequelize }
+
+// const { Sequelize } = require('sequelize');
+
+
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+//     dialect: 'mssql',
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialectOptions: {
+//         options: {
+//             encrypt: true,
+//             trustServerCertificate: true
+//         }
+//     }
+// })
+
+// module.exports = { sequelize }
